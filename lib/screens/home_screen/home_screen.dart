0a17d358ea8d443726735d38997c60c9202e2ba0
Isaias Cuvula <68303716+IsaiasCuvula@users.noreply.github.com>
2023-01-26 23:54:46 +0200

    watch list tile in separated widget

--- lib/screens/home_screen/home_screen.dart
+++ lib/screens/home_screen/home_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:movie_app/screens/home_screen/search_text_field.dart';
 import 'package:movie_app/screens/profile_screen.dart';
-import 'package:movie_app/screens/watch_list.dart';
+import 'package:movie_app/screens/watch_list/watch_list.dart';
 import 'package:provider/provider.dart';
 
 import '../../providers/movie_provider.dart';
--- lib/screens/watch_list/watch_list_tile.dart
+++ lib/screens/watch_list/watch_list_tile.dart
@@ -1,40 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:movie_app/models/movie.dart';
-import 'package:movie_app/providers/watch_list_movie_provider.dart';
-import 'package:movie_app/screens/detail_screen/icon_text_row.dart';
-import 'package:movie_app/utils/constants.dart';
-import 'package:provider/provider.dart';
 
-class WatchList extends StatelessWidget {
-  const WatchList({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Watch list'),
-      ),
-      body: Padding(
-        padding: kPadding20,
-        child: Consumer<WatchListMovieProvider>(
-          builder: (ctx, provider, _) {
-            final List<Movie> movies = provider.watchListMovie;
-            return ListView.separated(
-              itemBuilder: (ctx, index) {
-                final Movie movie = movies[index];
-                return WatchListMovieTile(movie: movie);
-              },
-              separatorBuilder: (ctx, index) {
-                return const SizedBox(height: 20.0);
-              },
-              itemCount: movies.length,
-            );
-          },
-        ),
-      ),
-    );
-  }
-}
+import '../../models/movie.dart';
+import '../detail_screen/icon_text_row.dart';
 
 class WatchListMovieTile extends StatelessWidget {
   const WatchListMovieTile({
