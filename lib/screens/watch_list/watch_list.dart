d0b0233e480bbff2cb16c0a44a8f5ddc968668af
Isaias Cuvula <68303716+IsaiasCuvula@users.noreply.github.com>
2023-01-27 04:15:50 +0200

    save movie in favorite and remove from favorite with dismiss endToStart

--- lib/screens/watch_list/watch_list.dart
+++ lib/screens/watch_list/watch_list.dart
@@ -19,16 +19,54 @@ class WatchList extends StatelessWidget {
         child: Consumer<WatchListMovieProvider>(
           builder: (ctx, provider, _) {
             final List<Movie> movies = provider.watchListMovie;
-            return ListView.separated(
-              itemBuilder: (ctx, index) {
-                final Movie movie = movies[index];
-                return WatchListMovieTile(movie: movie);
-              },
-              separatorBuilder: (ctx, index) {
-                return const SizedBox(height: 20.0);
-              },
-              itemCount: movies.length,
-            );
+            return movies.isEmpty
+                ? const Center(
+                    child: Text(
+                      'There is no movie\nin your watch list yet',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontSize: 20.0,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  )
+                : ListView.separated(
+                    itemBuilder: (ctx, index) {
+                      final Movie movie = movies[index];
+                      return Dismissible(
+                        key: Key('${movie.id}'),
+                        direction: DismissDirection.endToStart,
+                        background: Container(
+                          color: Colors.red,
+                          child: const Icon(
+                            Icons.delete,
+                            size: 28.0,
+                          ),
+                        ),
+                        onDismissed: (direction) {
+                          provider.removeMovieFromFavorite(movie);
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            SnackBar(
+                              content: Text(
+                                '${movie.title} removed from favourite',
+                              ),
+                              action: SnackBarAction(
+                                label: 'undo',
+                                onPressed: () {
+                                  provider.saveMovieToFavorite(movie);
+                                },
+                              ),
+                            ),
+                          );
+                        },
+                        child: WatchListMovieTile(movie: movie),
+                      );
+                    },
+                    separatorBuilder: (ctx, index) {
+                      return const SizedBox(height: 20.0);
+                    },
+                    itemCount: movies.length,
+                  );
           },
         ),
       ),
